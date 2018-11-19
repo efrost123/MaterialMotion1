d605e3340fa0c99daf46a97196c7fe6bd3f0fbef
Evgeny sdfdsfsdf <EFrost@inefwk.com>
2018-11-19 22:10:58 +0100

    Initial commit

---
+++ Source/MaterialTest1/MaterialTest1.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MaterialTest1.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MaterialTest1, "MaterialTest1" );
---
+++ Source/MaterialTest1/MaterialTest1GameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MaterialTest1GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MATERIALTEST1_API AMaterialTest1GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
